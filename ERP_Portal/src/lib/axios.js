666428b11337e63735cd0d95cec63784eec76f33
DEADCRATER <amssre.14835@gmail.com>
2026-04-04 14:58:01 +0530

    chore: update CORS origin and API base URL to production endpoints

--- ERP_Portal/src/lib/axios.js
+++ ERP_Portal/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://10.137.170.129:8000/api';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://erp-wdlf-git-main-deadcraters-projects.vercel.app/api';
 console.log(BASE_URL);
 
 const api = axios.create({
